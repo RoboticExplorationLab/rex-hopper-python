Using quaternions for rw control, still fixing mistakes

src/robotrunner.py
@@ -46,7 +46,7 @@ class Runner:
         self.dt = dt
         self.u = np.zeros(2)
         self.u_rw = np.zeros(3)
-        self.total_run = 500 # total_run
+        self.total_run = 1000 # total_run
         # height constant
 
         self.model = model
@@ -118,13 +118,14 @@ class Runner:
         rwzhist = np.zeros(total)
         err_sum = np.zeros(3)
         err_prev = np.zeros(3)
+        thetar = np.zeros(3)
         while steps < self.total_run:
             steps += 1
             t = t + self.dt
 
             # run simulator to get encoder and IMU feedback
-            q, q_dot, qrw, qrw_dot, b_quat, c, torque, f = self.simulator.sim_run(u=self.u, u_rw=self.u_rw)
-            b_orient = transforms3d.quaternions.quat2mat(b_quat)
+            q, q_dot, qrw, qrw_dot, Q_base, c, torque, f = self.simulator.sim_run(u=self.u, u_rw=self.u_rw)
+            b_orient = transforms3d.quaternions.quat2mat(Q_base)
             # enter encoder values into leg kinematics/dynamics
             self.leg.update_state(q_in=q)
 
@@ -158,7 +159,7 @@ class Runner:
 
             theta = np.array(transforms3d.euler.mat2euler(b_orient, axes='sxyz'))
             # theta[0] -= np.pi
-            # theta = transforms3d.quaternions.quat2axangle(b_quat)  # ax-angle representation
+            # theta = transforms3d.quaternions.quat2axangle(Q_base)  # ax-angle representation
 
             phi = np.array(transforms3d.euler.mat2euler(b_orient, axes='szyx'))[0]
             c_phi = np.cos(phi)
@@ -212,7 +213,7 @@ class Runner:
                 # self.u = -self.controller.wb_control(leg=self.leg, target=self.target, b_orient=b_orient, force=None)
 
             if self.model["model"] == 'design_rw':
-                self.u_rw, err_sum, err_prev = rw.rw_control(self.dt, x_ref, theta, omega, err_sum, err_prev)
+                self.u_rw, err_sum, err_prev, thetar = rw.rw_control(self.dt, Q_base, err_sum, err_prev)
 
             prev_state = state
 
@@ -221,7 +222,7 @@ class Runner:
             tau0hist[steps - 1] = torque[0]  # self.u[0]
             tau2hist[steps - 1] = torque[2]  # self.u[1]
             phist[steps - 1] = p_base_z
-            thetahist[steps - 1, :] = theta
+            thetahist[steps - 1, :] = thetar
             rw1hist[steps - 1] = torque[4]
             rw2hist[steps - 1] = torque[5]
             rwzhist[steps - 1] = torque[6]

src/rw.py
@@ -4,36 +4,46 @@ Copyright (C) 2021 Benjamin Bokser
 
 import numpy as np
 
-def rw_control(dt, x_ref, theta, omega, err_sum, err_prev):
+import utils
+
+def rw_control(dt, Q_base, err_sum, err_prev):
     """
     simple reaction wheel control
+    TODO: Add speed control inner PID loop
     """
-    # TODO: Add speed control inner PID loop
-    # print(x_ref[0:2], theta[0:2])
-    ku = 25
-    # Tu =
-    kp = ku # 0.6*ku
-    kd = 0 # kp*0.075
-    ki = 0
-    kpz = 50
-    kdz = kpz*0.2
+    x_ref = np.zeros(3)
+
+    a = -45 * np.pi / 180
+    b = 45 * np.pi / 180
+    Q_a = np.array([np.cos(a / 2), 0, 0, np.sin(a / 2)]).T
+    Q_1 = utils.L(Q_a) @ Q_base
+    Q_1 = Q_1 / (np.linalg.norm(Q_1))
+    theta_1 = 2 * np.arcsin(Q_1[1]) # x-axis of rotated body quaternion
+
+    Q_b = np.array([np.cos(b / 2), 0, 0, np.sin(b / 2)]).T
+    Q_2 = utils.L(Q_b) @ Q_base
+    Q_2 = Q_2 / (np.linalg.norm(Q_2))
+    theta_2 = 2 * np.arcsin(Q_2[1])  # x-axis of rotated body quaternion
+
+    theta_3 = 2 * np.arcsin(Q_base[3])  # z-axis of body quaternion
+
+    theta = np.array([theta_1, theta_2, theta_3])
+
+    ku = 20
+
+    kp = np.zeros((3, 3))
+    kd = np.zeros((3, 3))
+    ki = np.zeros((3, 3))
+
+    np.fill_diagonal(kp, [-ku, ku, 10])  # ku*0.6
+    np.fill_diagonal(kd, [0, 0, 10*0.002]) # ku*1.2/tu
+    np.fill_diagonal(ki, [0, 0, 0])  # ku*3*tu/40
 
     err = theta[0:3] - x_ref[0:3]
-    '''
-    tau_xdes = kp * err[0] + kd * (omega[0] - x_ref[6]) + ki*err_sum[0]*ts
-    tau_ydes = kp * err[1] + kd * (omega[1] - x_ref[7]) + ki*err_sum[1]*ts
-    tau_zdes = kpz * err[2] + kdz * (omega[2] - x_ref[8]) + ki*err_sum[2]*ts
-    '''
-    err_diff = (err-err_prev)/dt
-    tau_xdes = kp * err[0] + kd * err_diff[0] + ki*err_sum[0]*dt
-    tau_ydes = kp * err[1] + kd * err_diff[1] + ki*err_sum[1]*dt
-    tau_zdes = kpz * err[2] + kdz * err_diff[2] + ki*err_sum[2]*dt
 
-    u_rw = np.zeros(3)
-    u_rw[0] = (tau_xdes + tau_ydes) / (2 * np.sin(45))
-    u_rw[1] = (tau_xdes - tau_ydes) / (2 * np.sin(45))
-    u_rw[2] = tau_zdes
+    err_diff = (err-err_prev)/dt
+    u_rw = kp @ err + kd @ err_diff / dt + ki @ err_sum * dt
 
     err_sum += err
     err_prev = err
-    return u_rw, err_sum, err_prev
+    return u_rw, err_sum, err_prev, theta

src/simulationbridge.py
@@ -148,7 +148,7 @@ class Sim:
 
         base_or_p = np.array(p.getBasePositionAndOrientation(self.bot)[1])
         # pybullet gives quaternions in xyzw format instead of wxyz, so you need to shift values
-        b_quat = np.roll(base_or_p, 1)  # move last element to first place
+        Q_base = np.roll(base_or_p, 1)  # move last element to first place
         q = np.zeros(self.numJoints)
         q_dot = np.zeros(self.numJoints)
         qrw = np.zeros(2)
@@ -228,4 +228,4 @@ class Sim:
         if useRealTime == 0:
             p.stepSimulation()
 
-        return q, q_dot, qrw, qrw_dot, b_quat, c, torque, f
+        return q, q_dot, qrw, qrw_dot, Q_base, c, torque, f

src/utils.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def hat(omega):
+def hat(w):
     # skew-symmetric
-    return np.array([[0, - omega[3], omega[2]],
-                     [ω[3], 0, - ω[1]],
-                     [-ω[2], ω[1], 0]])
+    return np.array([[0, - w[2], w[1]],
+                     [w[2], 0, - w[0]],
+                     [-w[1], w[0], 0]])
 
 def L(Q):
     LQ = np.zeros((4, 4))
@@ -15,26 +15,25 @@ def L(Q):
     return LQ
 
 def R(Q):
-    LQ = np.zeros((4, 4))
-    LQ[0, 0] = Q[0]
-    LQ[0, 1:4] = - np.transpose(Q[1:4])
-    LQ[1:4, 0] = Q[1:4]
-    LQ[1:4, 1:4] = Q[0]*np.eye(3) - hat(Q[1:4])
+    RQ = np.zeros((4, 4))
+    RQ[0, 0] = Q[0]
+    RQ[0, 1:4] = - np.transpose(Q[1:4])
+    RQ[1:4, 0] = Q[1:4]
+    RQ[1:4, 1:4] = Q[0]*np.eye(3) - hat(Q[1:4])
     return RQ
 
 H = np.zeros((4, 4))
 H[1:4, 1:4] = np.eye(3)
 
-T = zeros(4)
+T = np.zeros((4, 1))
 T = np.fill_diagonal(T, [1.0, -1.0, -1.0, -1.0])
 
 def Expq(phi):
-
     # The quaternion exponential map ϕ → q
-    # Q = np.zeros(4)
-    theta = np.norm(phi)
-    Q = np.array([np.cos(theta / 2), 0.5 * phi * np.sinc(theta / (2 * np.pi))])
-
+    Q = np.zeros(4)
+    theta = np.linalg.norm(phi)
+    Q[0] = np.cos(theta / 2)
+    Q[1:4] = 0.5 * phi * np.sinc(theta / (2 * np.pi))
     return Q
 
 def Z(Q, p):
@@ -43,10 +42,10 @@ def Z(Q, p):
 
 def anglesolve(Q):
     # convert arbitrary quaternion to unsigned angle
-    return 2 * np.atan2(np.norm(Q[2:4]), Q[1])
+    return 2 * np.atan2(np.linalg.norm(Q[1:4]), Q[0])
 
 def angle_y(Q1, Q2):
     # signed angle about y axis of y-axis-constrained quaternions
     Q12 = L(Q1).T * Q2
-    Q12 = Q12 / (np.norm(Q12))
-    return 2 * np.asin(Q12[3])
+    Q12 = Q12 / (np.linalg.norm(Q12))
+    return 2 * np.arcsin(Q12[2])